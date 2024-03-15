1e2fa771f9ff315dab22a58b7ccfd8d2beb29fd8
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-15 22:59:41 +0200

    Update theme to use dark mode and add text to splash view

--- lib/Features/splash_view/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash_view/presentation/views/widgets/splash_view_body.dart
@@ -11,6 +11,10 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsDate.splashImage),
+        const Text(
+          'Read your free books',
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
