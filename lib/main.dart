be5e1be64a762c02bdf5c3fb540c15d147643b86
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-23 15:00:40 +0200

    Update font sizes and add Google Fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:booklyapp/Features/splash_view/presentation/views/splash_view.da
 import 'package:booklyapp/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -12,7 +13,8 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor,
+textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       debugShowCheckedModeBanner: false,
       home:const SplashView(),
     );
