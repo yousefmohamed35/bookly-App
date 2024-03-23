3ac08850fe76299e7efb98276f649b1e1c392ee6
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-23 14:26:55 +0200

    Add Styles class for text styling

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:booklyapp/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/Features/splash_view/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:booklyapp/Features/splash_view/presentation/views/widgets/feature_books_list_view.dart';
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeatureBooksListView(),
-      ],
+    return const Padding(
+      padding:  EdgeInsets.only(left: 16),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureBooksListView(),
+          SizedBox(height: 50,),
+          Text('Best Sellers',style: Styles.titleMedium,),
+        ],
+      ),
     );
   }
 }
