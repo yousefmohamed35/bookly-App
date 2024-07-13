567e2512ddb20f7556b89b82d1f48929de0e19db
yousefmohamed35 <yousefmo2021@gmail.com>
2024-07-13 17:58:58 +0300

    add device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:booklyapp/Features/splash_view/presentation/views/splash_view.dart';
 import 'package:booklyapp/constants.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const BooklyApp(),
+  ));
 }
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor,
-textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+    return GetMaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       debugShowCheckedModeBanner: false,
-      home:const SplashView(),
+      home: const SplashView(),
     );
   }
 }
