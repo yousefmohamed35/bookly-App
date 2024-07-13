35b3db03347d6408764a147e5da0259a2f9bdd59
yousefmohamed35 <yousefmo2021@gmail.com>
2024-07-13 21:28:18 +0300

    best seller item part 3

--- lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
@@ -24,6 +24,7 @@ class BestSellerListViewItem extends StatelessWidget {
           ),
           const SizedBox(width: 30),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * .5,
@@ -33,6 +34,24 @@ class BestSellerListViewItem extends StatelessWidget {
                   overflow: TextOverflow.ellipsis,
                   style: Styles.textStyle20,
                 ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                'j.k. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Row(
+                children: [
+                  Text(
+                    '19.99 \$',
+                    style: Styles.textStyle20,
+                  )
+                ],
               )
             ],
           ),
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -7,7 +7,7 @@ abstract class Styles {
   );
   static const textStyle20 = TextStyle(
     fontSize: 20,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.bold,
   );
   static const textStyle14 = TextStyle(
     fontSize: 14,
