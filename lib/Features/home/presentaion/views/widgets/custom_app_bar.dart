7e37b8b02cd34a834d2c62f085ec577de672bfc9
yousefmohamed35 <yousefmo2021@gmail.com>
2024-07-20 23:08:38 +0300

    create custom search text field

--- lib/Features/home/presentaion/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentaion/views/widgets/custom_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -9,7 +10,7 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 32),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
         child: Row(
           children: [
             Image.asset(
@@ -18,10 +19,12 @@ class CustomAppBar extends StatelessWidget {
             ),
             const Spacer(),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push('/searchView');
+              },
               icon: const Icon(
                 FontAwesomeIcons.magnifyingGlass,
-                size: 24,
+                size: 22,
               ),
             )
           ],
--- lib/Features/search/presentaion/views/widgets/search_view_body.dart
+++ lib/Features/search/presentaion/views/widgets/search_view_body.dart
@@ -1,10 +1,19 @@
+import 'package:booklyapp/Features/search/presentaion/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
   }
 }
