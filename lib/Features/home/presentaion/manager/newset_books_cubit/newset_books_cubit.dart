02a7757387db19f785271ca315c0bab174f06418
yousefmohamed35 <yousefmo2021@gmail.com>
2024-07-26 15:32:11 +0300

    newset books cubit done

--- lib/Features/home/presentaion/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentaion/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,24 @@
-import 'package:bloc/bloc.dart';
 import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homerepo) : super(NewsetBooksInitial());
+  final HomeRepo homerepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoadind());
+    var result = await homerepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(
+        NewsetBooksFailure(errorMessage: failure.errorMessage),
+      );
+    }, (books) {
+      emit(
+        NewsetBooksSuccess(books: books),
+      );
+    });
+  }
 }
