54ca3f29514a7cb194aa56a0a13385c004ec39a1
yousefmohamed35 <yousefmo2021@gmail.com>
2024-07-26 15:29:05 +0300

    create newset books cubit states

---
+++ lib/Features/home/presentaion/manager/newset_books_cubit/newset_books_state.dart
@@ -0,0 +1,21 @@
+part of 'newset_books_cubit.dart';
+
+sealed class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
+final class NewsetBooksLoadind extends NewsetBooksState {}
+final class NewsetBooksSuccess extends NewsetBooksState {
+  final List<BookModel> books;
+
+  const NewsetBooksSuccess({required this.books });
+}
+final class NewsetBooksFailure extends NewsetBooksState {
+  final String errorMessage;
+
+  const NewsetBooksFailure( {required this.errorMessage });
+}
